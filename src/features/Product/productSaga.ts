c01633bb50ec245169654ac35b8b8376ed76d74b
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-07-26 23:29:28 +0700

    fix-redux

--- src/features/Product/productSaga.ts
+++ src/features/Product/productSaga.ts
@@ -1,14 +1,17 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { call, takeLatest } from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 import { productApi } from "../../apis/productApi";
 import { Product } from "../../models";
 import { ListParams, ListResponse } from "../../models/common";
-import { getProducts } from "./productSlice";
+import { getProducts, getProductsFail, getProductsSuccess } from "./productSlice";
 
 function* handleFetchProducts(action: PayloadAction<ListParams>) {
-  console.log(action.payload);
-  const data: ListResponse<Product> = yield call(productApi.getProducts, action.payload);
-  console.log(data);
+  try {
+    const data: ListResponse<Product> = yield call(productApi.getProducts, action.payload);
+    yield put(getProductsSuccess(data));
+  } catch (error) {
+    yield put(getProductsFail());
+  }
 }
 
 export default function* productSaga() {
